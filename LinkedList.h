50638ea3eb61b41d1137a88e0530cace9ebcebfb
Michael Cooper <mic159@gmail.com>
2012-12-09 20:52:26 +1100

    Adding LinkedList2 that uses intrusive nodes

--- LinkedList.h
+++ LinkedList.h
@@ -55,11 +55,66 @@ public:
 		Node* ret = n->next;
 		delete n;
 		length -= 1;
-		return n->next;
+		return ret;
 	}
 
 	Node* last;
 	Node* first;
+	uint16_t length;
+};
+
+
+template<typename NodeType>
+class LinkedList2{
+public:
+	LinkedList2():last(NULL),first(NULL),length(0){}
+
+	void Add(NodeType* item) {
+		if (last == NULL) {
+			last = item;
+			first = item;
+			item->next = NULL;
+			item->prev = NULL;
+		} else {
+			last->next = item;
+			item->prev = last;
+			item->next = NULL;
+			last = item;
+		}
+		length += 1;
+	}
+	
+	template<typename SearchParamType>
+	NodeType*	Find(SearchParamType input, bool(*search_func)(const NodeType*, SearchParamType))
+	{
+		NodeType* current = first;
+		while (current != NULL)	{
+			if (search_func(current, input))
+				return current;
+			current = current->next;
+		}
+		return NULL;
+	}
+
+	NodeType* Remove(NodeType* n) {
+		if (!n)
+			return NULL;
+		if (first == n)
+			first = n->next;
+		if (last == n)
+			last = n->prev;
+		if (n->prev)
+			n->prev->next = n->next;
+		if (n->next)
+			n->next->prev = n->prev;
+		NodeType* ret = n->next;
+		delete n;
+		length -= 1;
+		return ret;
+	}
+	
+	NodeType* last;
+	NodeType* first;
 	uint16_t length;
 };
 
